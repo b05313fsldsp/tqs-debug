4742ad4ebc4c932324025342a9f23a005380e2a8
b05313fsldsp <b05313fsldsp@gmail.com>
2021-08-05 13:57:07 +0800

    Try POST and has some issue, marked dc- to check

--- index.js
+++ index.js
@@ -2,15 +2,20 @@
 import express from "express";
 import bodyParser from "body-parser";
 
-//import usersRoutes from "./routes/users.js";
+import usersRoutes from "./routes/users.js";
 
 const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.json());
 
-//app.use("/people", usersRoutes);
-app.get("/", (req, res) => res.send("Welcome to the Users API!"));
-//app.all("*", (req, res) =>res.send("You've tried reaching a route that doesn't exist."));
+app.use("/users", usersRoutes);
+
+app.get("/",(req, res) => {
+    console.log("[TEST]");
+    res.send("hello, wlecome to the homepage"); });
+
+app.all("*", (req, res) =>res.send("You've tried reaching a route that doesn't exist."));
+
 
 app.listen(PORT, () =>console.log(`Server running on port: http://localhost:${PORT}`));
